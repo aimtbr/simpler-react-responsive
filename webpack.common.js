31f8441075ff68cdf70f7adb1a54b96bcb0c1034
Maxim Marchuk <maxim.marchuk.d@gmail.com>
2021-11-07 01:47:15 +0200

    Move the npm package to the 'package' folder

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'package'),
     filename: '[name].js',
     publicPath: '/',
   },
